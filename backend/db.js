2ebf95805f058e497cf1ea8481e5fcb2eb477c57
TheJackHacker143 <paridaj819@gmail.com>
2026-09-19 11:21:17 +0530

    feat(backend): configure database connection and schema

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+// this file makes connection with mysql database
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10
+});
+
+// using promise wrapper so we can use async/await
+const db = pool.promise();
+
+module.exports = db;
